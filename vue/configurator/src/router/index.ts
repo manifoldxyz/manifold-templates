08b13b5d35700c0f7c20397814eeb01f75e40bac
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2023-02-07 17:50:59 -0500

    feat: add identity widget to configurator

--- vue/configurator/src/router/index.ts
+++ vue/configurator/src/router/index.ts
@@ -2,19 +2,31 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: "/",
+    redirect: "/connect",
+  },
+  {
     path: "/connect",
     name: "Connet Widget",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/ConnectView.vue"),
+      import(/* webpackChunkName: "connect" */ "../views/ConnectView.vue"),
   },
   {
     path: "/marketplace",
     name: "Marketplace Widget",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (marketplace.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/MarketplaceView.vue"),
+      import(
+        /* webpackChunkName: "marketplace" */ "../views/MarketplaceView.vue"
+      ),
+  },
+  {
+    path: "/identity",
+    name: "Wallet Identity Widget",
+    component: () =>
+      import(/* webpackChunkName: "identity" */ "../views/IdentityView.vue"),
   },
 ];
 
