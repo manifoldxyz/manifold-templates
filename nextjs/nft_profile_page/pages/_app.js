df10720189f2f2c17f4cc6e8231d9744a9a41e66
David Gan <dgan979@gmail.com>
2023-02-07 17:04:50 -0800

    update: nextjs nft profile template to use document

--- nextjs/nft_profile_page/pages/_app.js
+++ nextjs/nft_profile_page/pages/_app.js
@@ -1,31 +1,9 @@
 import '../styles/globals.css'
-import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  useEffect(() => { 
-    const handleRouteChange = (url, { shallow }) => {
-      // Note: need to trigger a m-refresh-widgets event every time the URL changes client side
-      window.dispatchEvent(new Event("m-refresh-widgets"));
-    }
-    router.events.on('routeChangeStart', handleRouteChange)
-  
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method:
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange)
-    }
-  }, [])
-
-  return (
-    <div>
-      {/* -- Stylesheets for connect and marketplace widgets -- */}
-      <link rel="stylesheet" href="https://connect.manifoldxyz.dev/2.2.1/connect.css"></link>
-      <script src="https://connect.manifoldxyz.dev/2.2.1/connect.umd.js" async></script>
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
---
+++ nextjs/nft_profile_page/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* -- Stylesheets for connect and marketplace widgets -- */}
+        {/* replace the specific version with `latest` to get the most up-to-date version of the widgets. */}
+        {/* WARNING: `latest` may cause breakage, so use with caution */}
+        <link rel="stylesheet" href="https://connect.manifoldxyz.dev/2.2.1/connect.css" />
+        <script src="https://connect.manifoldxyz.dev/2.2.1/connect.umd.js" async></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
